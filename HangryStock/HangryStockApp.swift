115c3a9008a149f2ed3e2fdea7a2d4559e6f51e7
fred M <fredmilloh@icloud.com>
2022-09-13 15:05:10 +0200

    first commit

---
+++ HangryStock/HangryStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangryStockApp.swift
+//  HangryStock
+//
+//  Created by fred M on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HangryStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
